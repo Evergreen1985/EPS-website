e2448a166eba07495797ec86701b18650584c43b
Evergreen1985 <gowrisainathreddy@gmail.com>
2026-04-18 05:37:43 -0700

    Add Supabase environment variables to config

--- next.config.js
+++ next.config.js
@@ -10,8 +10,10 @@ const nextConfig = {
     ],
   },
   env: {
-    GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
-    GOOGLE_PLACE_ID: process.env.GOOGLE_PLACE_ID,
+    GOOGLE_MAPS_API_KEY:            process.env.GOOGLE_MAPS_API_KEY,
+    GOOGLE_PLACE_ID:                process.env.GOOGLE_PLACE_ID,
+    NEXT_PUBLIC_SUPABASE_URL:       process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY:  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
   },
 };
 
